453d38b106dc5f79ed99b291609c4314ccaeeeef
V. Akio Isawa <vitorakioisawa@gmail.com>
2024-08-21 19:08:09 -0300

    Update Projeto_1.py

--- Projeto_1.py
+++ Projeto_1.py
@@ -2,14 +2,14 @@
 # cnd -> condicao 1 : pinhao 1 e coroa 1
 # cnd -> condicao 2 : pinhao 2 e coroa 2
 
-potencia_Hp        = 5
+pot_Hp             = 5
 cnd_1_rpm_min      = 500
 cnd_1_rpm_min      = 600
 cnd_2_rpm          = 100
-angulo_pressao_phi = 20
-modulo_coroas_mm   = 3
-num_dentes_coroa_1 = 60
-num_dentes_pinhao_2= 43 
+ang_pres_phi       = 20
+mod_gears_mm       = 3
+num_dent_n_g1      = 60
+num_dent_n_p2      = 43 
 
 material           = 'aco'
 lim_res_fadg_Se_Mpa= 700    #dado do slide de teoria de dalha por fadiga
@@ -18,13 +18,20 @@ E_Gpa              = 190
 v                  = 0.27
 grau_AGMA          = 1
 dureza_final_HB    = 350
-tempo_vida_anos    = 5
+temp_vida_anos     = 5
 temp_operacao_C    = 121
 confiabilidade     = 90     # %
-razao_vel_par_1_m_v= 0.8
+razao_vel_m_v1     = 0.8
 
 #   Determinar a distancia entre centros dos pares engrenados, visando manter o paralelismo entre os pares
 
 # a partir da razao de velocidades mv na condicao 1
 
-razao_engr_m_g = razao_vel_par_1_m_v
+razao_engr_mg_1   = 1 / razao_vel_m_v1
+num_dent_n_p1     = num_dent_n_g1 / razao_engr_mg_1
+
+diam_prim_dp_g1   = mod_gears_mm * num_dent_n_g1
+paso_diam_pd_1    = num_dent_n_g1 / diam_prim_dp_g1
+
+diam_prim_dp_p1   = diam_prim_dp_g1 * razao_vel_m_v1
+diam_prim_dp_p1   = num_dent_n_p1 / paso_diam_pd_1
