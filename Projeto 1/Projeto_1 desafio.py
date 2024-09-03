b9b7d64c4c9c1a8ea984922e752f36c312fce79b
V. Akio Isawa <vitorakioisawa@gmail.com>
2024-09-03 00:38:28 -0300

    entrega

--- Projeto 1/Projeto_1 desafio.py
+++ Projeto 1/Projeto_1 desafio.py
@@ -42,42 +42,54 @@ def rad2angle(rad):
     return rad * 180/ math.pi
 
 Distancia_entre_centros_C   = 162.0  
-diam_prim_dp_g1_mm          = 180  
-diam_prim_dp_p1_mm          = 144.0
-raio_prim_rp_g_mm           = diam_prim_dp_g1_mm / 2
-raio_prim_rp_p_mm           = diam_prim_dp_p1_mm / 2
-n_dentes_p1                 = 48
-diam_prim_dp_p1_pol         = mm2pol(diam_prim_dp_p1_mm)
-passo_diam                  = n_dentes_p1 / diam_prim_dp_p1_pol
-adendo_a_pol                = 1/passo_diam
-adendo_a_mm                 = pol2mm(adendo_a_pol)
+diam_prim_dp_g_mm           = 180.0
+raio_prim_rp_g_mm           = diam_prim_dp_g_mm / 2
+n_dentes_p                  = 48.0
+diam_prim_dp_p_mm           = 144.0
+raio_prim_rp_p_mm           = diam_prim_dp_p_mm / 2 
 
-# atraves da nova distancia entre centros, conseguimos obter o novo angulo de pressao
+# nova dist entre eixos
 Distancia_entre_centros_C_nova  = Distancia_entre_centros_C * 1.02
-raio_prim_rp_p_mm_novo          = Distancia_entre_centros_C_nova - raio_prim_rp_g_mm
-novo_cos    = (raio_prim_rp_p_mm + raio_prim_rp_g_mm)/Distancia_entre_centros_C_nova
-novo_phi = math.acos(novo_cos)
+
+# calculo do novo raio primitivo do pinhao, a engrenagem utilizada e a mesma
+raio_prim_rp_p_mm_novo      = Distancia_entre_centros_C_nova - raio_prim_rp_g_mm
+diam_prim_dp_p1_mm_novo     = 2 * raio_prim_rp_p_mm_novo
+diam_prim_dp_p1_pol_novo    = mm2pol(diam_prim_dp_p1_mm_novo)
+
+# calculo do angulo de pressao do par engrenado
+novo_cos                    = (raio_prim_rp_p_mm - 3.75 + raio_prim_rp_g_mm - 3.75 )/Distancia_entre_centros_C_nova
+novo_phi                    = math.acos(novo_cos)
+
+# Calculos a partir do par engrenado
+passo_diametral_novo        = n_dentes_p  / diam_prim_dp_p1_pol_novo
+adendo_a_pol_novo           = 1           / passo_diametral_novo
+dedendo_b_pol_novo          = 1.25        / passo_diametral_novo
+adendo_a_mm_novo            = pol2mm(adendo_a_pol_novo)
+dedendo_b_mm_novo           = pol2mm(dedendo_b_pol_novo)
+
+novo_raio_base_pinhao_1     = raio_prim_rp_p_mm - dedendo_b_mm_novo
+novo_raio_base_engrenagem_1 = raio_prim_rp_g_mm - dedendo_b_mm_novo
 
 # a partir do novo angulo de pressao, obtemos a linha Z
-z_1               = (raio_prim_rp_p_mm_novo + adendo_a_mm) ** 2 - (raio_prim_rp_p_mm_novo * math.cos(novo_phi)) ** 2
-z_2               = (raio_prim_rp_g_mm + adendo_a_mm) ** 2 - (raio_prim_rp_g_mm * math.cos(novo_phi)) ** 2 
-linha_acao_Z      = (z_1 ** 0.5) + (z_2 ** 0.5) - Distancia_entre_centros_C_nova * math.sin(novo_phi)
+z_1                         = (novo_raio_base_pinhao_1 + adendo_a_mm_novo) ** 2 - (novo_raio_base_pinhao_1 * math.cos(novo_phi)) ** 2
+z_2                         = (raio_prim_rp_g_mm       + adendo_a_mm_novo) ** 2 - (raio_prim_rp_g_mm       * math.cos(novo_phi)) ** 2 
+linha_acao_Z                = ( z_1 ** 0.5 ) + ( z_2 ** 0.5 )   -   Distancia_entre_centros_C_nova * math.sin(novo_phi)
 
 
 #Par engrenado 1--------------------------#
-paso_circ_pc      = math.pi * diam_prim_dp_p1_mm / n_dentes_p1
+paso_circ_pc                = math.pi      * diam_prim_dp_p1_mm_novo / n_dentes_p
 
 #Par engrenado 1--------------------------#
-paso_base_pb      = paso_circ_pc * math.cos(novo_phi)
+paso_base_pb                = paso_circ_pc * math.cos(novo_phi)
 
 #Par engrenado 1--------------------------#
 # com o novo passo base e anova linha de acao Z, obtemos a razao de contato
-razao_contato_mp  = linha_acao_Z / paso_base_pb
+razao_contato_mp            = linha_acao_Z / paso_base_pb
 
 
-print("o novo angulo de pressao obtido foi : "      ,   round(rad2angle(novo_phi),2))
-print("o raio primitivo engrenagem obtido foi : "   ,   round(raio_prim_rp_g_mm,1))
-print("o raio primitivo pinhao obtido foi : "       ,   round(raio_prim_rp_p_mm_novo,1))
-print("a nova razao de contato obtida foi : "       ,   round(razao_contato_mp,3))
+print("o novo angulo de pressao obtido foi :    "   ,   round(rad2angle(novo_phi),2)    , "mm")
+print("o raio primitivo engrenagem obtido foi : "   ,   round(raio_prim_rp_g_mm,1)      , "mm")
+print("o raio primitivo pinhao obtido foi :     "   ,   round(raio_prim_rp_p_mm_novo,1) , "mm")
+print("a nova razao de contato obtida foi :     "   ,   round(razao_contato_mp,3))
 
 
